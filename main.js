73d277700e9743b8081f67bc5297d57b8864d6aa
Lucas David Erkana <lderkana21x@gmail.com>
2022-11-16 10:57:38 +0200

    A dynamic section

--- main.js
+++ main.js
@@ -18,53 +18,99 @@ window.addEventListener('scroll', () => {
   navigation.classList.remove('nav-toggle');
 });
 
-const popupCards = [
+const cards = [
   {
-  title:"Tonic",
-  datas:{
-    author: "CANOPY",
-    role: 'Back End Dev',
-    year:'2015'},
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  skills: ['html5', 'css3', 'javascript', 'React'],
-  image:'svg_files/Nature.png',
-  liveLink:'See live',
-  sourceLink:'See Source',
-  },
-  {
-    title:"Multi-Post Stories",
-    datas:{
-      author: "CANOPY",
-      role: 'Back End Dev',
-      year:'2015'},
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    skills: ['html5', 'css3', 'javascript', 'React'],
-    image:'svg_files/Professional.svg',
-    liveLink:'See live',
-    sourceLink:'See Source',
+    id: 'cardOne',
+    title: 'Tonic',
+    education: ['CANOPY', '&nbsp; Back End Dev', '&nbsp; 2015'],
+    description: ' Microverse Module 02 Project: This is education project that allow user to add desire books to the list and also delete books. It makes use of local storage to preserve the data on the web browser',
+    technologies: ['html5', 'css3', 'javascript', 'ES6'],
+    image: {
+      mainImg: 'svg_files/Nature.png',
+      counter: './svg_files/Counter.png',
     },
-    {
-      title:"Facebook 360",
-      datas:{
-        author: "CANOPY",
-        role: 'Back End Dev',
-        year:'2015'},
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      skills: ['html5', 'css3', 'javascript', 'React'],
-      image:'svg_files/Explore.png',
-      liveLink:'See live',
-      sourceLink:'See Source',
-      },
-      {
-        title:"Uber Navigation",
-        datas:{
-          author: "CANOPY",
-          role: 'Back End Dev',
-          year:'2015'},
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-        skills: ['html5', 'css3', 'javascript', 'React'],
-        image:'svg_files/Availability.png',
-        liveLink:'See live',
-        sourceLink:'See Source',
-        }, 
-]
+    button: 'See Project',
+  },
+
+  {
+    id: 'cardTwo',
+    title: 'Multi-Post Stories',
+    education: ['FACEBOOK', '&nbsp; Back End Dev', '&nbsp; 2015'],
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    technologies: ['html', 'css', 'javascript'],
+    image: {
+      mainImg: 'svg_files/desktop-2.png',
+      counter: './svg_files/Counter.png',
+    },
+    button: 'See Project',
+    orderClas: 'card-twos',
+  },
+
+  {
+    id: 'cardThree',
+    title: 'Facebook 360',
+    education: ['CANOPY', '&nbsp; Back End Dev', '&nbsp; 2015'],
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    technologies: ['html', 'css', 'javascript'],
+    image: {
+      mainImg: 'svg_files/desktop-middle.png',
+      counter: './svg_files/Counter.png',
+    },
+    button: 'See Project',
+  },
+
+  {
+    id: 'cardfour',
+    title: 'Uber Navigation',
+    education: ['Uber', '&nbsp; Back End Dev', '&nbsp; 2015'],
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['html', 'css', 'javascript'],
+    image: {
+      mainImg: 'svg_files/desktop-last.png',
+      counter: './svg_files/Counter.png',
+    },
+    button: 'See Project',
+    orderClas: 'card-twos',
+  },
+];
+
+const body = document.querySelector('#portfolio');
+const section = document.createElement('section');
+section.className='container-2';
+body.appendChild(section);
+
+cards.forEach((object) => {
+  section.innerHTML += `<div class="card">
+  <img class="img ${object.orderClas}" src="${object.image.mainImg}" alt="Snapshoot Portfolio image">
+  <div class="section_content">
+  <h2 class="cardheading">
+   ${object.title}
+  </h2>
+  <ul class="minihead">
+  <li class="cano">
+    ${object.education[0]}
+  </li>
+  <li> <img src="${object.image.counter}" alt="counter image">${object.education[1]}</li>
+  <li> <img src="${object.image.counter}" alt="counter image">${object.education[2]}</li>
+  </ul>
+  <p>
+    ${object.description}
+  </p>
+  <ul class="used-lang">
+      <li>
+        ${object.technologies[0]}
+      </li>
+      <li>
+         ${object.technologies[1]}
+      </li>
+      <li>
+         ${object.technologies[2]}
+      </li>
+  </ul>
+  <button id="${object.id}" data-id="${object.id}" class="button" type="button">
+    ${object.button}
+  </button>
+  </div>
+  </div>`;
+});
+
