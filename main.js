3cc0ce412b1f6189a24219469e25e4f2036e8bf4
Kelvin <ngotho.kelvin@yahoo.com>
2022-11-17 19:00:40 +0300

    store data function

--- main.js
+++ main.js
@@ -64,6 +64,13 @@ const nameData = document.querySelector('#name');
 const messageData = document.querySelector('#message');
 const emptyObject = {};
 
+function storeData() { 
+   emptyObject.namee = nameData.value;
+   emptyObject.email = emailE.value; 
+   emptyObject.message = messageData.value;
+   localStorage.setItem('object', JSON.stringify(emptyObject)); 
+  } 
+
 
 
 
