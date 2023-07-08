011a54024574a57c6c08f8edac22cc6a43722ba7
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-08 19:02:52 +0200

    Fix loading speed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
 
-const Loader = () => {
+const CanvasLoader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
-  )
-}
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className='canvas-loader'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
 
-export default Loader
+export default CanvasLoader;
