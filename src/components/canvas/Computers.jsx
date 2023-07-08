4a35721207bf2723dcb06a9f5188929b1e47536d
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-08 19:56:42 +0200

    Latest

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -55,7 +55,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameLoop='demand'
+      frameloop='demand'
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
