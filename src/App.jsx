8eeca3be9b3f48d896d6948452d757a2aa476f47
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-08 16:58:11 +0200

    Import components and BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
 const App = () => {
   return (
-    <div>
-     Lucas Erkana Portfolio
-    </div>
+    <BrowserRouter> 
+    
+    </BrowserRouter>
      )
 }
 
