ce23bf412cd9001e95dc3aaabea646cbbe36f4f9
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-08 17:47:11 +0200

    aspply tailwind css
    npm install --legacy-peer-deeps -D postcss autoprefixer
    npx tailwindcss init -p

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
-    <BrowserRouter> 
-    
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
     </BrowserRouter>
-     )
+  );
 }
 
-
-
-export default App
+export default App;
