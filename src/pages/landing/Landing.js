56b618835162e99bc852dab2bffdf9fd3a48e700
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-13 22:48:41 +0200

    Update Landing.js

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 import landingImage from "../../images/portfolio1.png";
-import Draw from "../../components/Draw";
 import SocialIcons from "../../components/SocialIcons";
 
 const Landing = ({ name }) => {
